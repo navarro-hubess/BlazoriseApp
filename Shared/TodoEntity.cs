10aa1b11d491ddfc1269e712c6c1a02f4cc28af4
navarrowebapi <navarro.fabio@gmail.com>
2020-08-20 18:21:38 -0300

    inicial

---
+++ Shared/TodoEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorAppProject.Shared
+{
+    public class TodoEntity
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
